898d0190cdf7c907e878c2489fa7f5c7db9a2ad1
Jonathan Zimmermann <zimmz92@gmail.com>
2021-07-25 18:14:13 -0400

    Added Vk instance creation and extension checking

--- Arundos.cpp
+++ Arundos.cpp
@@ -13,8 +13,9 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+#include <string>
 //#include <cmath>
-//#include <string>
 
 #include "Arundos.h"
 
@@ -33,7 +34,7 @@ public:
 	}
 
 private:
-
+	VkInstance instance;
 	GLFWwindow* window;
 
 	void initWindow() {
@@ -45,7 +46,7 @@ private:
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	}
 	void initVulkan() {
-
+		createInstance();
 	}
 
 	void mainLoop() {
@@ -55,9 +56,65 @@ private:
 	}
 
 	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
+
 		glfwDestroyWindow(window);
 
 		glfwTerminate();
+	}
+
+	void createInstance() {
+		// Base information structure for Vulkan Application
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Arundos";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		// Extensions required by glfw to build the window
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		// Check if required extensions are supported by the hardware
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		bool extsUnavail = false;
+
+		for (int i = 0; i < glfwExtensionCount; i++) {
+			bool extUnavail = true;
+			for (const auto& extention : extensions) {
+				if (strcmp( glfwExtensions[i], extention.extensionName) == 0) {
+					extUnavail = false;
+				}
+			}
+			if (extUnavail) {
+				std::cout << "REQUIRED EXTENTION " << glfwExtensions[i] << " UNAVAILABLE!" << '\n';
+				extsUnavail = true;
+			}
+		}
+
+		if (extsUnavail) {
+			throw std::runtime_error("One or more required extentions are unavailable!");
+		}
+
+		// Create Vulkan Instance
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
 	}
 
 };
