68f6176e6d7f7608d35346622ae2c590ba550d14
Jonathan Zimmermann <zimmz92@gmail.com>
2021-09-25 06:26:15 -0400

    Moved shaders into a graphics engine location and updated the makefile accordingly.

---
+++ engines/graphics/shaders/compile.bat
@@ -0,0 +1,3 @@
+C:/VulkanSDK/1.2.182.0/Bin32/glslc.exe C:\Users\halo9\Source\Repos\arundos\Engines\Graphics\shaders\shader.vert -o C:\Users\halo9\Source\Repos\arundos\Engines\Graphics\shaders\vert.spv
+C:/VulkanSDK/1.2.182.0/Bin32/glslc.exe C:\Users\halo9\Source\Repos\arundos\Engines\Graphics\shaders\shader.frag -o C:\Users\halo9\Source\Repos\arundos\Engines\Graphics\shaders\frag.spv
+pause
