a259de40d981f8c1ac8843f728b56523d2743efc
jzimmermann <zimmz92@gmail.com>
2022-02-08 08:38:45 -0500

    Added autogeneration of the .spv files into the build routine

--- engines/graphics/shaders/compile.bat
+++ engines/graphics/shaders/compile.bat
@@ -1,3 +1,2 @@
-C:/VulkanSDK/1.2.182.0/Bin32/glslc.exe C:\Users\halo9\Source\Repos\arundos\Engines\Graphics\shaders\shader.vert -o C:\Users\halo9\Source\Repos\arundos\Engines\Graphics\shaders\vert.spv
-C:/VulkanSDK/1.2.182.0/Bin32/glslc.exe C:\Users\halo9\Source\Repos\arundos\Engines\Graphics\shaders\shader.frag -o C:\Users\halo9\Source\Repos\arundos\Engines\Graphics\shaders\frag.spv
-pause
+C:/VulkanSDK/1.2.182.0/Bin32/glslc.exe C:\Users\jzimmermann\source\repos\Arundos\engines\graphics\shaders\shader.vert -o C:\Users\jzimmermann\source\repos\Arundos\engines\graphics\shaders\vert.spv
+C:/VulkanSDK/1.2.182.0/Bin32/glslc.exe C:\Users\jzimmermann\source\repos\Arundos\engines\graphics\shaders\shader.frag -o C:\Users\jzimmermann\source\repos\Arundos\engines\graphics\shaders\frag.spv
