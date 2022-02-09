17bcefc5fc4ed61fd5e8e0bb1337611cb6817a98
Jonathan Zimmermann <zimmz92@gmail.com>
2022-02-09 07:50:34 -0500

    Added vulkan as relative path so correct pointers can be made for both windows/linux/custom user install environment.

--- engines/graphics/shaders/compile.bat
+++ engines/graphics/shaders/compile.bat
@@ -1,2 +1,2 @@
-C:/VulkanSDK/1.2.182.0/Bin32/glslc.exe %~1\engines\graphics\shaders\shader.vert -o %~1\engines\graphics\shaders\vert.spv
-C:/VulkanSDK/1.2.182.0/Bin32/glslc.exe %~1\engines\graphics\shaders\shader.frag -o %~1\engines\graphics\shaders\frag.spv
+%~2/glslc.exe %~1\engines\graphics\shaders\shader.vert -o %~1\engines\graphics\shaders\vert.spv
+%~2/glslc.exe %~1\engines\graphics\shaders\shader.frag -o %~1\engines\graphics\shaders\frag.spv
