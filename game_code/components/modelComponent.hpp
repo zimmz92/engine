ef1bc9c3751788c7e88788a2cb94a12e54477ff1
Jonathan Zimmermann <zimmz92@gmail.com>
2023-01-17 16:54:11 -0500

    Removed the translation member since it is actually just the position of the entity.

--- game_code/components/modelComponent.hpp
+++ game_code/components/modelComponent.hpp
@@ -1,31 +1,62 @@
+/*! \file modelComponent.hpp
+    \brief The script defining the model component.
+
+    The model compoent is defined and the instance for the game is declared.
+
+*/
 #pragma once
 
 #include "ae_ecs.hpp"
 #include "ae_model.hpp"
 
 namespace ae {
+
+    //! The model component structure.
+    /*!
+    * This structure defines the model data stored for each entity using the model component.
+    */
     struct modelComponentStructure {
-        // Pointer to the entities model file
+
+        //! The model pointer.
+        /*!
+        * A pointer to the model used by a entity.
+        */
         std::shared_ptr<AeModel> m_model{};
 
-        // The orientation and size that the model displays as
-        glm::vec3 translation{}; // (position offset)
+        //! The scale of the model.
+        /*!
+        * Defines the scaling of the model used by a entity.
+        */
         glm::vec3 scale{ 1.0f, 1.0f, 1.0f };
+
+        //! The rotation of the model.
+        /*!
+        * Defines the rotation of the model used by a entity.
+        */
         glm::vec3 rotation;
 
-        // Matrix corrsponds to Translate * Ry * Rx * Rz * Scale
-        // Rotations correspond to Tait-bryan angles of Y(1), X(2), Z(3)
-        // https://en.wikipedia.org/wiki/Euler_angles#Rotation_matrix
-        glm::mat4 mat4();
-        glm::mat3 normalMatrix();
 
         // TODO Placeholder for texture
     };
 
-    class positionComponentClass : public ae_ecs::AeComponent<modelComponentStructure> {
+
+    //!  The model component class.
+    /*!
+    * The model component class is derived from the AeComponent template class using the model component structure.
+    */
+    class modelComponentClass : public ae_ecs::AeComponent<modelComponentStructure> {
     public:
-        positionComponentClass(ae_ecs::AeComponentManager& t_componentManager) : AeComponent(t_componentManager) {};
-        ~positionComponentClass() {};
+        //!  The constructor of the modelComponent class.
+        /*!
+        * The modelComponentClass constructor uses the AeComponent constructor with no addtions.
+        */
+        modelComponentClass(ae_ecs::AeComponentManager& t_componentManager) : AeComponent(t_componentManager) {};
+
+        //!  The destructor of the modelComponent class.
+        /*!
+        * The modelComponentClass destructor uses the AeComponent constructor with no addtions.
+        */
+        ~modelComponentClass() {};
 
     private:
 
@@ -33,5 +64,9 @@ namespace ae {
 
     };
 
-    positionComponentClass positionComponent(ae_ecs::ecsComponentManager);
+    //!  The instansiation of the model component for use by the game.
+    /*!
+    * The instansiation of the model component for use by the game.
+    */
+    modelComponentClass modelComponent(ae_ecs::ecsComponentManager);
 }
