ca4944ff85f85644d9cc911a673f1b1888398ca6
Jonathan Zimmermann <zimmz92@gmail.com>
2023-07-19 21:01:04 -0400

    Moved the component manager member function implementations into the implementation files.

--- engines/ecs/ae_component_manager.cpp
+++ engines/ecs/ae_component_manager.cpp
@@ -41,4 +41,81 @@ namespace ae_ecs {
 
 		}
 	};
+
+	// Gets the number of available components that can be allocated by returning the current value of the top of stack
+	// pointer and adding one since the stack array starts at 0.
+	ecs_id AeComponentManager::getAvailableComponents() {
+		return m_componentIdStackTop + 1;
+	};
+
+	// Returns the component signature of a specific entity from the entity component signature array.
+	std::bitset<MAX_NUM_COMPONENTS + 1>  AeComponentManager::getComponentSignature(ecs_id t_entityId) {
+		return m_entityComponentSignatures[t_entityId];
+	};
+
+	// Sets the entity component signature bit to indicate that the entity uses the component.
+	void AeComponentManager::setEntityComponentSignature(ecs_id t_entityId, ecs_id t_componentId) {
+		m_entityComponentSignatures[t_entityId].set(t_componentId);
+		// TODO: When the entity sets a component, and is live, force the component manager to update applicable systems lists of valid entities to act upon
+	};
+
+	// Resets the entity component signature bit to indicate that the entity does not use the component.
+	void AeComponentManager::unsetEntityComponentSignature(ecs_id t_entityId, ecs_id t_componentId) {
+		m_entityComponentSignatures[t_entityId].reset(t_componentId);
+		// TODO: When the entity removes a component force the component manager to update applicable systems lists of valid entities to act upon
+	};
+
+	// Set the last bit of the entityComponentSignature high to indicate that the Entity is enabled and systems can work
+	// on it.
+	void AeComponentManager::enableEntity(ecs_id t_entityId) {
+		m_entityComponentSignatures[t_entityId].set(MAX_NUM_COMPONENTS);
+		// TODO: When the entity is set live, force the component manager to update applicable systems lists of valid entities to act upon
+	};
+
+	// Unset the last bit of the entityComponentSignature, low, to indicate that the Entity is disabled and systems
+	// should not work on it.
+	void AeComponentManager::disableEntity(ecs_id t_entityId) {
+		m_entityComponentSignatures[t_entityId].reset(MAX_NUM_COMPONENTS);
+		// TODO: When the entity is disabled, force the component manager to update applicable systems lists of valid entities to act upon
+	};
+
+	// Check to see if the bit in the entityComponentSignature of the entity is set high that corresponds to the
+	// component. If high then the component is used by the entity.
+	bool AeComponentManager::isComponentUsed(ecs_id t_entityId, ecs_id t_componentId) {
+		return m_entityComponentSignatures[t_entityId].test(t_componentId);
+	};
+
+	// Removes the entity by clearing the component signature of the entity so the next entity that is allocated the
+	// same ID as the one to be removed it will not have the same components, and component data, by default.
+	void AeComponentManager::removeEntity(ecs_id t_entityId){
+		m_entityComponentSignatures[t_entityId].reset();
+	};
+
+	// Sets the system component signature bit to indicate that the system uses the component.
+	void AeComponentManager::setSystemComponentSignature(ecs_id t_systemId, ecs_id t_componentId) {
+		m_systemComponentSignatures[t_systemId].set(t_componentId);
+	};
+
+	// Resets the system component signature bit to indicate that the system does not use the component.
+	void AeComponentManager::unsetSystemComponentSignature(ecs_id t_systemId, ecs_id t_componentId) {
+		m_systemComponentSignatures[t_systemId].reset(t_componentId);
+	};
+
+	// TODO: Implement this function
+	void AeComponentManager::removeSystem(ecs_id t_systemId) {
+
+	};
+
+	// TODO: Implement function that compares system component signatures to the entity component signatures and returns a list of valid entities to the systems to act upon.
+	void AeComponentManager::updateSystemsEntities() {
+
+	};
+
+	//  TODO: Document the implementation
+	template <class T>
+	const ecs_id AeComponentManager::allocateComponentTypeId() {
+		static const ecs_id staticTypeId{ componentIdCount++ };
+		return staticTypeId;
+	};
+
 }
--- engines/ecs/ae_component_manager.hpp
+++ engines/ecs/ae_component_manager.hpp
@@ -37,90 +37,63 @@ namespace ae_ecs {
         /// Gets the number of components that have not been allocated from the stack and are therefore available for
         /// use.
         /// \return Number of components still available to be used.
-		ecs_id getAvailableComponents() { return m_componentIdStackTop + 1; };
+		ecs_id getAvailableComponents();
 
         /// Gets the component signature for an entity.
         /// \param t_entityId The ID of the entity.
         /// \return A bitset array that indicates the components utilized by an entity.
-		std::bitset<MAX_NUM_COMPONENTS + 1>  getComponentSignature(ecs_id t_entityId) {
-            return m_entityComponentSignatures[t_entityId];
-        };
+		std::bitset<MAX_NUM_COMPONENTS + 1>  getComponentSignature(ecs_id t_entityId);
 
         ///  A function that sets the field in the entity component signature corresponding to the specific component.
         /// \param t_entityId  The ID of the entity.
         /// \param t_componentId The ID of the component to be added as used for the entity.
-		void setEntityComponentSignature(ecs_id t_entityId, ecs_id t_componentId) {
-			m_entityComponentSignatures[t_entityId].set(t_componentId);
-            // TODO: When the entity sets a component, and is live, force the component manager to update applicable systems lists of valid entities to act upon
-		};
+		void setEntityComponentSignature(ecs_id t_entityId, ecs_id t_componentId);
 
         /// A function that unsets the field in the entity component signature corresponding to the specific component.
         /// \param t_entityId The ID of the entity.
         /// \param t_componentId The ID of the component to be removed as used for the entity.
-		void unsetEntityComponentSignature(ecs_id t_entityId, ecs_id t_componentId) {
-			m_entityComponentSignatures[t_entityId].reset(t_componentId);
-            // TODO: When the entity removes a component force the component manager to update applicable systems lists of valid entities to act upon
-		};
+		void unsetEntityComponentSignature(ecs_id t_entityId, ecs_id t_componentId);
 
         /// Enables entity allowing it to be acted upon by systems
         /// \param t_entityId The ID of the entity
-        void enableEntity(ecs_id t_entityId) {
-            m_entityComponentSignatures[t_entityId].set(MAX_NUM_COMPONENTS);
-            // TODO: When the entity is set live, force the component manager to update applicable systems lists of valid entities to act upon
-        };
+        void enableEntity(ecs_id t_entityId);
 
         /// Disables entity preventing it from being acted upon by systems
         /// \param t_entityId The ID of the entity
-        void disableEntity(ecs_id t_entityId) {
-            m_entityComponentSignatures[t_entityId].reset(MAX_NUM_COMPONENTS);
-            // TODO: When the entity is disabled, force the component manager to update applicable systems lists of valid entities to act upon
-        };
-
-
-        void removeEntity(ecs_id t_entityId);
+        void disableEntity(ecs_id t_entityId);
 
         /// Checks to see if an entity uses a component.
         /// \param t_entityId The ID of the entity
         /// \param t_componentId The ID of the component.
         /// \return true if the entity uses the component.
-        bool isComponentUsed(ecs_id t_entityId, ecs_id t_componentId) {
-            return m_entityComponentSignatures[t_entityId].test(t_componentId);
-        };
+        bool isComponentUsed(ecs_id t_entityId, ecs_id t_componentId);
 
-        /// TODO: Destroy an Entity
-
-
+        /// TODO: Document the usage of this function
+        void removeEntity(ecs_id t_entityId);
 
         ///  A function that sets the field in the system component signature corresponding to the specific component.
         /// \param t_systemId The ID of the system.
         /// \param t_componentId The ID of the component to be added as required for the system.
-        void setSystemComponentSignature(ecs_id t_systemId, ecs_id t_componentId) {
-            m_systemComponentSignatures[t_systemId].set(t_componentId);
-        };
+        void setSystemComponentSignature(ecs_id t_systemId, ecs_id t_componentId);
 
         ///  A function that unsets the field in the system component signature corresponding to the specific component.
         /// \param t_systemId The ID of the system.
         /// \param t_componentId The ID of the component to be removed as required for the system.
-        void unsetSystemComponentSignature(ecs_id t_systemId, ecs_id t_componentId) {
-            m_systemComponentSignatures[t_systemId].reset(t_componentId);
-        };
+        void unsetSystemComponentSignature(ecs_id t_systemId, ecs_id t_componentId);
 
-        /// TODO: Destroy a system
+        /// TODO: Document the usage of this function
+        void removeSystem(ecs_id t_systemId);
 
         /// Compares system component signatures to the entity component signatures and returns a list of valid entities
         /// to the systems to act upon.
-        void updateSystemsEntities() {
-            // TODO: Create a class that compares system component signatures to the entity component signatures and returns a list of valid entities to the systems to act upon.
-        }
+        /// TODO: Document the usage of this function
+        void updateSystemsEntities();
 
 		/// Function to allocate an ID to a specific component class so every component spawned from that class can be identified.
 		/// \tparam T The component class being allocated an ID.
 		/// \return The component class ID.
 		template <class T>
-		static const ecs_id allocateComponentTypeId() {
-			static const ecs_id staticTypeId{ componentIdCount++ };
-			return staticTypeId;
-		};
+		static const ecs_id allocateComponentTypeId();
 
 	private:
 
