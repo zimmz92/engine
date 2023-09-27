bed7b451350c0a470afb3448494def23b5c622d1
jzimmermann <zimmz92@gmail.com>
2023-09-26 21:24:14 -0400

    Fixed an error where the vulkan API version as 1.0 when the application is being built using vulkan API version 1.3.

--- engines/graphics/ae_device.cpp
+++ engines/graphics/ae_device.cpp
@@ -92,7 +92,7 @@ namespace ae {
 		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.pEngineName = "Arundos Engine";
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.apiVersion = VK_API_VERSION_1_0;
+		appInfo.apiVersion = VK_API_VERSION_1_3;
 
         // Base information for the instance to be created, with the app information.
 		VkInstanceCreateInfo createInfo{};
