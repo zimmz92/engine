cad58b5702ca62dc4d784ee1db0d564383888fec
Jonathan Zimmermann <zimmz92@gmail.com>
2023-12-08 14:45:24 -0500

    Refactored constants to no longer be #defines so that it can be assured they are using a known type.

--- engines/ecs/ae_ecs_constants.hpp
+++ engines/ecs/ae_ecs_constants.hpp
@@ -2,14 +2,14 @@
 /// \brief The script defining constants used by the ecs.
 /// Define the constants used by the ecs.
 
-// These constants must come before the includes to ensure that they are available when compiling the included files below
-#define MAX_NUM_COMPONENTS 32
-#define MAX_NUM_ENTITIES 16384
-#define MAX_NUM_SYSTEMS 32
-
 #pragma once
 
 #include <cstdint>
+#include <limits>
 
-using ecs_id = std::int64_t;
-using ecs_systemInterval = std::int64_t;
+using ecs_id = std::size_t;
+using ecs_systemInterval = std::size_t;
+
+static const ecs_id MAX_NUM_COMPONENTS = 32;
+static const ecs_id MAX_NUM_ENTITIES = 65536;
+static const ecs_id MAX_NUM_SYSTEMS = 32;
