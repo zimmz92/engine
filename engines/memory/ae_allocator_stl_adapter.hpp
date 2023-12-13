df538215d45be35da74dc12ab9f13f8bf906f960
jzimmermann <zimmz92@gmail.com>
2023-12-12 20:48:31 -0500

    Worked on cleaning up the code and commenting

--- engines/memory/ae_allocator_stl_adapter.hpp
+++ engines/memory/ae_allocator_stl_adapter.hpp
@@ -14,26 +14,30 @@ namespace ae_memory {
     class AeAllocatorStlAdaptor
     {
     public:
-
-        typedef T value_type;
-
-
+        /// Remove the default constructor.
         AeAllocatorStlAdaptor() = delete;
 
-        AeAllocatorStlAdaptor(Alloc& allocator) noexcept : m_allocator(allocator){
+        /// Create the allocator stl library adapter by specifying which allocator shall be actually used for memory
+        /// allocation and deallocation.
+        /// \param t_allocator The allocator which will actually be handling memory allocation and deallocation.
+        AeAllocatorStlAdaptor(Alloc& t_allocator) noexcept : m_allocator(t_allocator){
 
         };
 
+        /// Copy constructor for the adapter.
+        /// \param t_allocatorStlAdaptor The allocator stl adapter to be copied into a new AeAllocatorStlAdaptor.
         template<typename U>
-        AeAllocatorStlAdaptor(const AeAllocatorStlAdaptor<U, Alloc>& other) noexcept : m_allocator(other.m_allocator){
+        AeAllocatorStlAdaptor(const AeAllocatorStlAdaptor<U, Alloc>& t_allocatorStlAdaptor) noexcept : m_allocator(t_allocatorStlAdaptor.m_allocator){
 
         };
 
-        [[nodiscard]] constexpr T* allocate(std::size_t n)
+        /// Passthrough to the allocate function of the allocator this adapter is the stl interface for.
+        /// \param t_numElements The number of elements of type T memory is to be allocated for.
+        /// \return A pointer to the start of the allocated memory for number of elements of type T.
+        [[nodiscard]] constexpr T* allocate(std::size_t t_numElements)
         {
-            return reinterpret_cast<T*>
-            (m_allocator.Allocate(n * sizeof(T), alignof(T)));
-        }
+            return reinterpret_cast<T*>(m_allocator.Allocate(t_numElements * sizeof(T)));
+        };
 
         constexpr void deallocate(T* p, [[maybe_unused]] std::size_t n)
         noexcept
