0a8d0ee19bb03e25cf7d9acc1879e08a52edec92
jzimmermann <zimmz92@gmail.com>
2024-08-19 22:00:21 -0400

    Continuing to work on creating a parallel compatible radix sort algorithm.

--- engine/library/radix_sort.hpp
+++ engine/library/radix_sort.hpp
@@ -93,7 +93,7 @@ namespace ae {
         const std::uint32_t t_numCores = 16;
         const std::uint32_t radix_bits = 4;
         const std::uint32_t radix_hist = 2 << radix_bits;
-        const std::uint32_t num_hists = 8;
+        const std::uint32_t num_hists = 8; // Number of radix bits in max value of array members
 
         // ensure array can be split into sections
         if (t_arraySize % t_numCores) {
@@ -102,69 +102,48 @@ namespace ae {
         }
         const std::uint32_t range_size = t_arraySize / t_numCores;
 
-        std::uint32_t hist[t_numCores][num_hists][radix_hist] = {0};
+        std::uint32_t hist[num_hists][t_numCores][radix_hist] = {0};
+        std::uint32_t offset[num_hists][t_numCores][radix_hist] = {0};
 
-        // For loop emulating number of cores working on different portions of the dataset
-        for (int i = 0; i < t_numCores; i++) {
+        // Repeat the algorithm for each of the histograms starting from LSB to MSB.
+        for(int j = 0; j < num_hists; j++){
 
-            // For loop emulating number of cores reading same dataset but creating histograms for different "LSB" Histograms
-            for(int j = 0; j < num_hists; j++){
+            // Emulate number of cores working on different portions of the dataset
+            for (int i = 0; i < t_numCores; i++) {
                 std::size_t start_idx = range_size * i;
-                std::size_t end_idx = range_size * (i+1);
-                for(std::size_t k = start_idx; k < end_idx; k++){
-                    hist[i][j][(t_arrayRef[k] >> (radix_bits * j)) & (radix_hist * (j != num_hists -1))]++;
+                std::size_t end_idx = range_size * (i + 1);
+
+                // Create histograms for different "LSB" Histograms, this is also done by different cores in parallel
+                for (std::size_t k = start_idx; k < end_idx; k++) {
+                    // Add one to the portion of the histogram corresponding to the array value masked by the radix.
+                    hist[i][j][(t_arrayRef[k] >> (radix_bits * j)) & (radix_hist * (j != num_hists - 1))]++;
+                }
+            }
+
+            // Calculate the offset for each of the histogram values.
+            for (int i = 0; i < t_numCores; i++) {
+                for (std::size_t k = 0; k < radix_hist; k++) {
+                    offset[i][j][k] = 0;
+
+                    // Loop through values of all cores and sum all the counts for values less than the current value.
+                    for(int lt_k = 0; lt_k<k; lt_k++){
+                        for(int all_cores = 0; all_cores<t_numCores; all_cores++){
+                            offset[i][j][k] += hist[all_cores][j][lt_k];
+                        }
+                    }
+
+                    // Loop through all values from cores that took ranges less than the range of the current core
+                    for(int lt_i = 0; lt_i<i; lt_i++){
+                        offset[i][j][k] += hist[lt_i][j][k];
+                    }
                 }
             }
         }
-
-        // Calculate the cumulative sum across the bins
-        std::uint32_t h0_sum = 0, h1_sum = 0, h2_sum = 0;
-        std::uint32_t temp_sum = 0;
-        for (int i = 0; i < radix_hist; i++) {
-            temp_sum = h0[i] + h0_sum;
-            // I don't like -1 being used here, will lead to an overflow and most likely will cause issues.
-            h0[i] = h0_sum - 1;
-            h0_sum = temp_sum;
-
-            temp_sum = h1[i] + h1_sum;
-            h1[i] = h1_sum - 1;
-            h1_sum = temp_sum;
-
-            temp_sum = h2[i] + h2_sum;
-            h2[i] = h2_sum - 1;
-            h2_sum = temp_sum;
-        }
-
-        // Sort the array based the 11-LS bits.
-        for (int i = 0; i < t_arraySize; i++) {
-
-            std::uint32_t element = t_arrayRef[i];
-            std::uint32_t pos = element & 0x7FF;
-
-            t_sorted[++h0[pos]] = element;
-        }
-
-        std::uint32_t temp_sort[t_arraySize];
-        // Sort the array based the next 11-LS bits.
-        for (int i = 0; i < t_arraySize; i++) {
-            std::uint32_t element = t_sorted[i];
-            std::uint32_t pos = (element >> 11) & 0x7FF;
-
-            temp_sort[++h1[pos]] = element;
-        }
-
-        // Sort the array based the 11-MS bits.
-        for (int i = 0; i < t_arraySize; i++) {
-            std::uint32_t element = temp_sort[i];
-            std::uint32_t pos = element >> 22;
-
-            t_sorted[++h2[pos]] = element;
-        }
-
+        printf("Done Parallel Sorting.\n");
     }
 
     void test_radix_sort(){
-        std::size_t const num_random_values = 10;
+        std::size_t const num_random_values = 32;
         std::random_device random_device;
         std::mt19937 random_engine(random_device());
         std::uniform_int_distribution<uint32_t> uint32_distribution(0, UINT32_MAX);
@@ -181,6 +160,9 @@ namespace ae {
 
         std::uint32_t sorted[num_random_values];
         lsb_radix_sort(numbers,sorted,num_random_values);
+
+        std::uint32_t parallel_sorted[num_random_values];
+        parallel_lsb_radix_sort(numbers,parallel_sorted,num_random_values);
 
         printf("Sorted Array:\n");
         for(size_t i = 0; i < num_random_values; ++i){
